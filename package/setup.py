914e88fa2dc454b533aa4b62b86aee9b02dadb5e
Pepe Marquez <pepemarquezprieto@gmail.com>
2024-06-04 12:14:03 +0200

    Add charge calculation of tolerance factor and charge balanced formulas examples. Added also references.

--- package/setup.py
+++ package/setup.py
@@ -30,6 +30,8 @@ setup(
         "langchainhub",
         "langchain-community",
         "langchain-openai",
+        "instructor",
+        "pymatgen",
     ],
     long_description="""
 # Style and Imports for the Structured Data Extraction Book
