a09173624b7f959e504a2870951592ea3172cb8e
MrtinoRG <martinriosgarcia@gmail.com>
2024-06-25 10:00:49 +0200

    First tests on the setup function

--- package/llmstructdata/__init__.py
+++ package/llmstructdata/__init__.py
@@ -1,8 +1,24 @@
+from os.path import (
+    join,
+    dirname
+)
+import sys
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from dotenv import (
+    load_dotenv,
+    find_dotenv,
+)
 import warnings
 
 mpl.style.use("book.mplstyle")
+
+# First and second are equivalent
+# Start by trying the most basic one
+load_dotenv(find_dotenv(sys.path[1]))
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path, override=True)
+# load_dotenv(".env", override=True)
 
 
 warnings.filterwarnings("ignore")
